6e0bfc554ee99ec57a156fda71bfe6d8b45d506c
Daniele Araújo <s.araujodaniele@gmail.com>
2026-03-22 16:45:34 -0300

    Refactor currency calculation to use integer centavos and improve output formatting

--- beecrowd/c/1018.c
+++ beecrowd/c/1018.c
@@ -1,83 +1,30 @@
-/* Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) 
-no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. 
-A seguir mostre o valor lido e a relação de notas necessárias.
+/* Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis
+(cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100,
+50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas
+necessárias.
 */
 
 #include <stdio.h>
-int main(){
 
-	double valor, troco;
-	int cem, cinquenta, vinte, dez, cinco, dois;
-	double um, m50, m25, m10, m5, m1;
+int main() {
+  int valor;
+  scanf("%d", &valor);
+  int original_valor = valor;
 
-	scanf("%lf",&valor);
+  int denomin[] = {100, 50, 20, 10, 5, 2, 1};
+  int quant[7] = {0};
+  const char* labels[] = {"100,00", "50,00", "20,00", "10,00",
+                          "5,00",   "2,00",  "1,00"};
 
-	troco = valor;
+  // Processa notas (reais)
+  for (int i = 0; i < 7; i++) {
+    quant[i] = valor / denomin[i];
+    valor %= denomin[i];
+  }
 
-	while (reais/100){
-		troco-= 100;
-		cont_100++;
-	}
-	while (reais/50){
-		reais -= 50;
-		cont_50++;
-	}
-	while (reais/20){
-		reais -= 20;
-		cont_20++;
-	}
-	while (reais/10){
-		reais -= 10;
-		cont_10++;
-	}
-	while (reais/5){
-		reais -= 5;
-		cont_5++;
-	}
-	while (reais/2){
-		reais -= 2;
-		cont_2++;
-	}
-	if (reais){
-		cont_1++;
-	}
+  printf("%d\n", original_valor);
+  for (int i = 0; i < 7; i++)
+    printf("%d nota(s) de R$ %s\n", quant[i], labels[i]);
 
-	// processamento da parte fracionária (centavos)
-	while (centavos/50){
-		centavos -= 50;
-		cont_50_cent++; 
-	}
-	while (centavos/25){
-		centavos -= 25;
-		cont_25_cent++;
-	}
-	while (centavos/10){
-		centavos -= 10;
-		cont_10_cent++;
-	}
-	while (centavos/5){
-		centavos -= 5;
-		cont_5_cent++;
-	}
-	while (centavos){
-		centavos--;
-		cont_1_cent++;
-	}
-    
-	printf("NOTAS:\n");
-	printf("%lf nota(s) de R$ 100.00\n", cem);
-	printf("%lf nota(s) de R$ 50.00\n", cinquenta);
-	printf("%lf nota(s) de R$ 20.00\n", vinte);
-	printf("%lf nota(s) de R$ 10.00\n", dez);
-	printf("%lf nota(s) de R$ 5.00\n", cinco);
-	printf("%lf nota(s) de R$ 2.00\n", dois);
-	printf("MOEDAS:\n");
-	printf("%lf moeda(s) de R$ 1.00\n", um);
-	printf("%lf moeda(s) de R$ 0.50\n", m50);
-	printf("%lf moeda(s) de R$ 0.25\n", m25);
-	printf("%lf moeda(s) de R$ 0.10\n", m10);
-	printf("%lf moeda(s) de R$ 0.05\n", m5);
-	printf("%lf moeda(s) de R$ 0.01\n", troco);
-	   
-	return(0);
+  return 0;
 }
--- beecrowd/c/1021.c
+++ beecrowd/c/1021.c
@@ -1,61 +1,38 @@
-/* Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário. 
-A seguir, calcule o menor número de notas e moedas possíveis no qual o valor pode ser decomposto. 
-As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. 
-A seguir mostre a relação de notas necessárias.
+/* Leia um valor de ponto flutuante com duas casas decimais. Este valor
+representa um valor monetário. A seguir, calcule o menor número de notas e
+moedas possíveis no qual o valor pode ser decomposto. As notas consideradas são
+de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e
+0.01. A seguir mostre a relação de notas necessárias.
 */
 
 #include <stdio.h>
-int main(){
 
-    double valor, troco;
-    double cem, cinquenta, vinte, dez, cinco, dois, um, m50, m25, m10, m5, m1;
+int main() {
+  double valor;
+  scanf("%lf", &valor);
 
-    scanf("%lf",&valor);
+  // Converte para centavos (inteiros) para evitar problemas com ponto flutuante
+  int centavos = (int)(valor * 100 + 0.5);
 
-    troco = valor;
+  // Arrays com denominações em centavos e rótulos
+  int denomin[] = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+  int quant[12] = {0};
+  const char* labels[] = {"100.00", "50.00", "20.00", "10.00", "5.00", "2.00",
+                          "1.00",   "0.50",  "0.25",  "0.10",  "0.05", "0.01"};
 
-    cem = troco / 100;
-    troco = troco - (cem *100);
+  // Calcula notas e moedas
+  for (int i = 0; i < 12; i++) {
+    quant[i] = centavos / denomin[i];
+    centavos %= denomin[i];
+  }
 
-    cinquenta = troco / 50;
-    troco = troco - (cinquenta * 50);
+  printf("NOTAS:\n");
+  for (int i = 0; i < 6; i++)
+    printf("%d nota(s) de R$ %s\n", quant[i], labels[i]);
 
-    vinte = troco / 20;
-    troco = troco - (vinte * 20);
+  printf("MOEDAS:\n");
+  for (int i = 6; i < 12; i++)
+    printf("%d moeda(s) de R$ %s\n", quant[i], labels[i]);
 
-    dez = troco /10;
-    troco = troco -(dez * 10);
-
-    cinco = troco / 5;
-    troco = troco - (cinco * 5);
-
-    dois = troco / 2;
-    troco = troco - (dois * 2);
-	
-	um = troco / 1;
-    troco = troco - (um * 1);
-    
-    m50 = troco / 0.50;
-    troco = troco - (m50 * 0.50);
-    
-    m25 = troco / 0.25;
-    troco = troco - (m25 * 0.25);
-    
-    m10 = troco / 0.10;
-    troco = troco - (m10 * 0.10);
-    
-    m5 = troco / 0.5;
-    troco = troco - (m5 * 0.25);
-    
-    printf("NOTAS:\n");
-    printf("%lf nota(s) de R$ 100.00\n", cem);
-    printf("%lf nota(s) de R$ 50.00\n", cinquenta);
-    printf("%lf nota(s) de R$ 20.00\n", vinte);
-    printf("%lf nota(s) de R$ 10.00\n", dez);
-    printf("%lf nota(s) de R$ 5.00\n", cinco);
-    printf("%lf nota(s) de R$ 2.00\n", dois);
-    printf("MOEDAS:\n");
-    printf("%lf moeda(s) de R$ 1.00\n", troco);
-
-   return(0);
+  return 0;
 }
